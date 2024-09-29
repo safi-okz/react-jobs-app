11e0139a526b6ca3b3b45e28bd28ab2dc5f8a7a3
safi-okz <safi90655@gmail.com>
2024-09-29 23:50:33 +0500

    start work on add job

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePages from './pages/HomePages';
 import MainLayout from './Layout/MainLayout';
 import JobsPage from './pages/JobsPage';
 import JobPage, { jobLoader } from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
 import NotFound from './pages/NotFound';
 
 const router = createBrowserRouter(
@@ -10,6 +11,7 @@ const router = createBrowserRouter(
       <Route path='/' element={<MainLayout />}>
         <Route index element={<HomePages />} />
         <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/add-job" element={<AddJobPage />} />
         <Route path="/jobs/:id" element={<JobPage />} loader={ jobLoader } />
         <Route path="*" element={<NotFound />} />
       </Route>
